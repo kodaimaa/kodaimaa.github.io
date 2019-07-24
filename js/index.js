098e341b24960a103e4cabbedb9ef728bf8d8a1b
Tra <kodaimaa@gmail.com>
2019-07-24 15:39:47 +0700

    init random color

--- js/index.js
+++ js/index.js
@@ -9,22 +9,35 @@ $(document).ready(function() {
 				if ($(this).hasClass('scale-out')) {
 			 		$(this).removeClass('scale-out');
 			 	}
-			} 
+			}
 		});
 	});
 });
 
-// // Select elems where 'attribute' ends with 'Dialog'
-// $("[attribute$='Dialog']"); 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-// // Selects all divs where attribute is NOT equal to value    
-// $("div[attribute!='value']"); 
+function setRandomColor() {
+  $("#colorpad").css("background-color", getRandomColor());
+}
+
+// // Select elems where 'attribute' ends with 'Dialog'
+// $("[attribute$='Dialog']");
+
+// // Selects all divs where attribute is NOT equal to value
+// $("div[attribute!='value']");
 
 // // Select all elements that have an attribute whose value is like
-// $("[attribute*='value']"); 
+// $("[attribute*='value']");
 
 // // Select all elements that have an attribute whose value has the word foobar
-// $("[attribute~='foobar']"); 
+// $("[attribute~='foobar']");
 
 // // Select all elements that have an attribute whose value starts with 'foo' and ends
 // //  with 'bar'
